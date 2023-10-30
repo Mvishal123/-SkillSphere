571c65c2672aae4f61f0357964738b6ecf23a7d5
Vishal-m <vishalmahesh.study@gmail.com>
2023-10-30 19:34:27 +0530

    fixed shadcn and tailwind :)

---
+++ packages/ui/index.tsx
@@ -0,0 +1,11 @@
+// shadcn-ui
+export * from "./src/ui/accordion";
+export * from "./src/ui/button";
+export * from "./src/ui/avatar";
+export * from "./src/ui/dropdown-menu";
+export * from "./src/ui/navigation-menu";
+export * from "./src/ui/popover";
+export * from "./src/ui/sheet";
+
+// common
+export * from "./src/common/Card";
