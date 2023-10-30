c0c8d3d193797a411a4e5c2cea337fe28c49f813
Vishal-m <vishalmahesh.study@gmail.com>
2023-10-31 00:28:47 +0530

    admin added

---
+++ apps/admin/src/components/Home.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react';
+
+import { Button } from "ui"
+
+const Home = () => {
+  return (
+    <div>
+        <Button>Home</Button>
+    </div>
+  )
+}
+
+export default Home
